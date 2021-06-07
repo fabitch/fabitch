1d3313179c410dca782531d6ae7084e6d1819ee2
Anne-Laura <adlig>
2021-06-07 10:16:45 +0200

    added chart titles

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from typing import List
-
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.express as px
+import pandas as pd
 
 import yfinance as yf
 
@@ -66,21 +65,31 @@ def create_card_div_for_graph(figure):
 
 @app.callback(
     Output(component_id='charts', component_property='children'),
-    Input(component_id='ticker', component_property='value')
+    Input(component_id='ticker', component_property='value'),
+    State(component_id='ticker', component_property='options')
 )
-def update_chart_one(ticker):
+def update_chart_one(ticker, label):
+    company_name = label[0]['label']
     charts = []
     # data = yf.Ticker(ticker)
     stock_prices = DATA.history('max')
+    title = f"Stock Prices for {company_name}"
+    fig = px.line(stock_prices, x=stock_prices.index,
+                  y=['Open', 'High', 'Close'], title=title)
+    charts.append(create_card_div_for_graph(fig))
+
+    title = f"Daily Returns of {company_name}"
     daily_returns = stock_prices['Close'].pct_change()
 
-    charts.append(create_card_div_for_graph(px.line(daily_returns)))
+    charts.append(create_card_div_for_graph(px.line(daily_returns, title=title)))
 
     return_dist = daily_returns.round(2).reset_index()
     return_dist = return_dist.groupby('Close').count()
 
     # TODO: add line chart to bar chart to see distribution
-    fig = px.bar(return_dist)
+    title = f"Distribution of daily Returns of {company_name} since " \
+            f"{min(pd.to_datetime(daily_returns.index))}"
+    fig = px.bar(return_dist, title=title)
     charts.append(create_card_div_for_graph(fig))
     return charts
 
